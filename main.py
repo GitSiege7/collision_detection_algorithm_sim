758a99144e14093123f6a8e847bd873347ddaa9e
GitSiege7 <carljklug@icloud.com>
2025-07-07 14:02:17 -0400

    groundwork

---
+++ main.py
@@ -0,0 +1,10 @@
+from constants import *
+import pygame
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    return
